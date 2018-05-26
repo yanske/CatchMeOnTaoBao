46aafc15dde0ae5d9b76c5a59221cd1896cc5219
yanske1 <yansongke1@gmail.com>
2018-05-26 11:31:12 -0400

    Redirects to Taobao link

--- yanske1/CatchMeOnTaoBao/functions/__main__.js
+++ yanske1/CatchMeOnTaoBao/functions/__main__.js
@@ -2,28 +2,27 @@
 * @param {string} name 
 * @returns {string}
 */
-module.exports = (name = 'Gucci_Belt', context, callback) => {
-  const https = require('https');
+
+module.exports = (name = 'Gucci Belt', context, callback) => {
   const translate = require('google-translate-api');
+  const https = require('https');
 
   let taobao_query = "https://s.taobao.com/search?q=";
 
   translate(name, {to: 'zh-CN'}).then(chinese => {
     let s = chinese.text;
-    var convert=function(str){
-      var codeStr="";
-      for(var i=0;i<str.length;i++){
-        var _code=str.charCodeAt(i).toString(16).toUpperCase();
-        codeStr+='\\u'+"0000".substr(0,4-_code.length)+_code
+    var convert = function(str) {
+      var codeStr = "";
+      for(var i = 0; i < str.length; i++){
+        var _code = str.charCodeAt(i).toString(16).toUpperCase();
+        codeStr += '\\u'+"0000".substr(0,4-_code.length) + _code
       }
+
       return codeStr;
     }
     
     convert(s);
     const taobao = taobao_query + s;
-
-    // Get the HTML page
-
-    callback(null, taobao_query + s);
+    callback(null, taobao);
   });
 };
